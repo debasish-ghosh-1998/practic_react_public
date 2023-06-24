a9141a4e91c48bd5c84fe987c6a742b9a853e00c
Debasish <debasish.ghosh@codeclouds.com>
2023-06-24 14:18:14 +0530

    added login feature

--- src/Components/Auth/Login.js
+++ src/Components/Auth/Login.js
@@ -1,35 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './assets/login.css';
 export const Login = () => {
-  return (
-    <div >
+    const [loginData,setLoginData] = useState(['']);
 
-<div className="wrapper fadeInDown mt-4">
-  <div id="formContent">
-  
+    const loginHandler = (event)=>{
+        event.preventDefault();
+        const email = document.getElementById('login').value;
+        const password = document.getElementById('password').value;
 
+        const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ email:email,password:password })
+        };
+        fetch('http://127.0.0.1:8000/api/login', requestOptions)
+            .then(response => response.json())
+            .then(data => console.log(data));
+      }
 
-    <div className="fadeIn first mt-4">
-      <h3>LOGIN</h3>
-    </div>
-
-   
-    <form>
-      <input type="text" id="login" className="fadeIn second" name="login" placeholder="login"/>
-      <input type="text" id="password" className="fadeIn third" name="login" placeholder="password"/>
-      <input type="submit" className="fadeIn fourth" value="Log In"/>
-    </form>
-
-    
-    <div id="formFooter">
-      <a className="underlineHover" href="#">Forgot Password?</a>
-    </div>
-
-  </div>
-</div>
-
-
-    </div>
-  )
+    return (
+        <div >
+            <div className="wrapper fadeInDown mt-4">
+                <div id="formContent">
+                    <div className="fadeIn first mt-4">
+                        <h3>LOGIN</h3>
+                    </div>
+                    <form onSubmit={loginHandler} >
+                        <input type="text" id="login" className="fadeIn second" name="login" placeholder="login" />
+                        <input type="text" id="password" className="fadeIn third" name="login" placeholder="password" />
+                        <input type="submit" className="fadeIn fourth" value="Log In" />
+                    </form>
+                    <div id="formFooter">
+                        <a className="underlineHover" href="#">Forgot Password?</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
